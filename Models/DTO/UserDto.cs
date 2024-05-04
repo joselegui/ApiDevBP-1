64f6872130af5987486faba184241b3d426d3cd5
Jose <leguizamonjoseramon@yahoo.com.ar>
2024-05-04 14:01:01 -0300

    Se agregan campos requeridos

--- Models/DTO/UserDto.cs
+++ Models/DTO/UserDto.cs
@@ -1,9 +1,13 @@
-﻿namespace ApiDevBP.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDevBP.Models.DTO
 {
     public class UserDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "The last name is mandatory")]
         public string Lastname { get; set; }
     }
 }
